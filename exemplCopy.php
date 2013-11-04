664c0055dd7f53ea70255b338d90b12114647d43
rasxod <st@ssmart.ru>
2013-11-04 15:31:27 +0300

    Update exemplCopy.php

--- exemplCopy.php
+++ exemplCopy.php
@@ -13,5 +13,5 @@
  *
  */
   
-  echo file_get_contents("http://wq8.ru/GetLic.php?name=copy&version=0.01a&mDate=2013&domain=wq8.ru&desc=wq8.ru - Лицензионное соглашение для Вашего сайта&target=_BLANK"); 
+  echo file_get_contents("http://wq8.ru/APIGetCopy.php?name=copy&version=0.01a&mDate=2013&domain=wq8.ru&desc=wq8.ru - Лицензионное соглашение для Вашего сайта&target=_BLANK"); 
 ?>
