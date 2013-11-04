0e72dc19a2f487f78052467eceb2e4dfa83a7123
rasxod <st@ssmart.ru>
2013-11-04 15:16:40 +0300

    Update exemplCopy_v0.0.1a.php

--- exemplCopy_v0.0.1a.php
+++ exemplCopy_v0.0.1a.php
@@ -3,12 +3,15 @@
  * Пример вызова копирайта 
  *  
  * Обязательные поля
- * $version - версия
+ * version - версия
  *
  * Опционально
- * $mDate - дата создания сайта(опционально)
+ * mDate - дата создания сайта(опционально)
+ * domain - домен вашего сайта
+ * desc - описание сайта
+ * target - цель открытия
  *
  */
   
-  echo file_get_contents("http://wq8.ru/GetLic.php?name=copy&version=".$version."&mDate=".$mDate); 
+  echo file_get_contents("http://wq8.ru/GetLic.php?name=copy&version=0.01a&mDate=2013&domain=wq8.ru&desc=wq8.ru - Лицензионное соглашение для Вашего сайта&target=_BLANK"); 
 ?>
